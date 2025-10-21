ce5cca49790fdc65fb1eb5f4e49a4515bd94e506
Attal <Attal@CoolGuy.lan>
2025-10-20 22:03:45 -0400

    Initial commit: Hello world

---
+++ src/seleniumHomework1/Helloworld.java
@@ -0,0 +1,11 @@
+package seleniumHomework1;
+
+import org.openqa.selenium.WebDriver;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+System.out.println("Hello world");
+	}
+
+}
